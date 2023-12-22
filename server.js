900e2eb0e6bc97b0b8b72fde6504a93fb742ea0d
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2023-12-22 11:08:12 -0500

    Updated port

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 
 
 app.use(express.static("public"));
@@ -8,5 +11,5 @@ app.get('/', function (req, res) {
     res.send("Hello World!")
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`This app is avalible on: http://localhost:${PORT}/`));
