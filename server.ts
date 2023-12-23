a995a30ed3c925c6856e0ee75976f5bbbe2f380c
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2023-12-23 17:42:55 -0500

    Database connected

--- server.ts
+++ server.ts
@@ -2,12 +2,11 @@ import express from "express";
 const app = express();
 import path from 'path';
 const __dirname = path.resolve();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV !== 'production') {
 	dotenv.config();
-}
-
+  };
 // Connect to db
 import { MongoClient } from "mongodb";
 
@@ -37,28 +36,28 @@ app.get("/", (req, res) => {
 app.get("/api/forecast/:startDate/:count", async (req, res) => {
 	let recordsToQuery: number = parseInt(req.params.count)
 	let startDate: Date = new Date(req.params.startDate);
-	if(isNaN(recordsToQuery) || isNaN(startDate.getTime())){
+	if (isNaN(recordsToQuery) || isNaN(startDate.getTime())) {
 		// Illegal argument
 	}
-	
-	try{
-	const database = client.db("WeatherApp");
-	const forecasts = database.collection<Forecast>("Forecasts");
-	const result: Array<Forecast> = [];
 
-	const query = { time: { $gt: startDate } };
-	const cursor = forecasts.find<Forecast>(query).limit(recordsToQuery);
-	if ((await forecasts.countDocuments(query)) === 0) {
-		console.warn("No documents found!");
+	try {
+		const database = client.db("WeatherApp");
+		const forecasts = database.collection<Forecast>("Forecasts");
+		const result: Array<Forecast> = [];
+
+		const query = { time: { $gt: startDate } };
+		const cursor = forecasts.find<Forecast>(query).limit(recordsToQuery);
+		if ((await forecasts.countDocuments(query)) === 0) {
+			console.warn("No documents found!");
+		}
+		for await (const doc of cursor) {
+			console.dir(doc);
+			result.push(doc);
+		}
+		res.json(result);
+	} catch (error) {
+		// Internal server error with database read
 	}
-	for await (const doc of cursor) {
-		console.dir(doc);
-		result.push(doc);
-	}
-	res.json(result);
-} catch (error) {
-	// Internal server error with database read
-}
 });
 
 // Open server
