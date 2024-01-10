6c4a4654c941fd5afda841cd1c304fd2dcb474ff
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2024-01-10 16:59:17 -0500

    Refactoring and comments

--- backend/Library/LSTMFunc.ts
+++ backend/Library/LSTMFunc.ts
@@ -3,84 +3,109 @@ import axios from "axios";
 import std from "../../models/LSTMjs/std.json";
 import mean from "../../models/LSTMjs/mean.json";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function getData(): Promise<any> {
-  const serverPromise = new Promise((resolve, reject) => {
+//TODO: move to seperate file
+export interface predictionResult {
+  predictedDegrees: number;
+  predictedSpeed: number;
+  predictedTemp: number;
+  actualDegrees: number;
+  actualSpeed: number;
+  actualTemp: number;
+}
+
+export default async function getData(): Promise<predictionResult> {
+  // Create promise
+  const serverPromise = new Promise<predictionResult>((resolve, reject) => {
     const url =
       "https://aviationweather.gov/api/data/metar/?ids=ksts&format=json&hours=25";
     const toReturn: number[] = [];
+    // Fetch data from aviationweather.gov
     axios
       .get(url)
       .then(async (response) => {
-        const dataArray = response.data;
-        const day = 24 * 60 * 60;
-        const year = 365.2425 * day;
+        try {
+          const dataArray = response.data;
+          const day = 24 * 60 * 60;
+          const year = 365.2425 * day;
 
-        for (let i = 1; i < 25; i++) {
-          let windDir = dataArray[i].wdir;
-          if (windDir == "VRB") {
-            windDir = 0;
-          }
-          const windSpeed = dataArray[i].wspd;
-          const temp = dataArray[i].temp;
-          const time = dataArray[i].obsTime;
+          for (let i = 1; i < 25; i++) {
+            let windDir = dataArray[i].wdir;
+            if (windDir == "VRB") {
+              windDir = 0;
+            }
+            const windSpeed = dataArray[i].wspd;
+            const temp = dataArray[i].temp;
+            const time = dataArray[i].obsTime;
 
-          toReturn.push((time - mean.DATE) / std.DATE);
-          toReturn.push(
-            (windSpeed * Math.cos((windDir * Math.PI) / 180) - mean.Wx) /
+            toReturn.push((time - mean.DATE) / std.DATE);
+            toReturn.push(
+              (windSpeed * Math.cos((windDir * Math.PI) / 180) - mean.Wx) /
               std.Wx,
-          );
-          toReturn.push(
-            (windSpeed * Math.sin((windDir * Math.PI) / 180) - mean.Wy) /
+            );
+            toReturn.push(
+              (windSpeed * Math.sin((windDir * Math.PI) / 180) - mean.Wy) /
               std.Wy,
-          );
-          toReturn.push(
-            (Math.sin(time * ((2 * Math.PI) / day)) - mean["Day sin"]) /
+            );
+            toReturn.push(
+              (Math.sin(time * ((2 * Math.PI) / day)) - mean["Day sin"]) /
               std["Day sin"],
-          );
-          toReturn.push(
-            (Math.cos(time * ((2 * Math.PI) / day)) - mean["Day cos"]) /
+            );
+            toReturn.push(
+              (Math.cos(time * ((2 * Math.PI) / day)) - mean["Day cos"]) /
               std["Day cos"],
-          );
-          toReturn.push(
-            (Math.sin(time * ((2 * Math.PI) / year)) - mean["Year sin"]) /
+            );
+            toReturn.push(
+              (Math.sin(time * ((2 * Math.PI) / year)) - mean["Year sin"]) /
               std["Year sin"],
-          );
-          toReturn.push(
-            (Math.cos(time * ((2 * Math.PI) / year)) - mean["Year cos"]) /
+            );
+            toReturn.push(
+              (Math.cos(time * ((2 * Math.PI) / year)) - mean["Year cos"]) /
               std["Year cos"],
+            );
+            toReturn.push((temp - mean.Temp) / std.Temp);
+          }
+          // Turn data into correctly shaped tensor
+          const tfArray = tf.tensor(toReturn);
+          const reshapedTensor = tfArray.reshape([1, 24, 8]);
+
+          // Load model and generate prediction
+          const model = await tf.loadLayersModel(
+            "http://localhost:8080/tfjs_artifacts/model.json",
           );
-          toReturn.push((temp - mean.Temp) / std.Temp);
+          const x = model.predict(reshapedTensor) as tf.Tensor;
+
+          x.array().then((array) => {
+            // De-normalize prediction
+            const predictedWx = array[0][23][0] * std.Wx + mean.Wx;
+            const predictedWy = array[0][23][1] * std.Wy + mean.Wy;
+
+            const predictedDegrees =
+              Math.atan2(predictedWx, predictedWy) * (180 / Math.PI);
+            const predictedSpeed = Math.sqrt(
+              Math.pow(predictedWx, 2) + Math.pow(predictedWy, 2),
+            );
+            const predictedTemp = array[0][23][2] * std.Temp + mean.Temp;
+
+            // Generate return object using predicted data
+            const obj: predictionResult = {
+              predictedDegrees,
+              predictedSpeed,
+              predictedTemp,
+              actualDegrees: dataArray[0].wdir,
+              actualSpeed: dataArray[0].wspd,
+              actualTemp: dataArray[0].temp,
+            };
+            resolve(obj);
+
+          })
+            // Handle all errors
+            .catch((error) => {
+              console.log(error);
+              reject("An error occured while predicting data");
+            });
+        } catch (error) {
+          reject("an error occurred while processing third party data.");
         }
-
-        const tfArray = tf.tensor(toReturn);
-        const reshapedTensor = tfArray.reshape([1, 24, 8]);
-        const model = await tf.loadLayersModel(
-          "http://localhost:8080/tfjs_artifacts/model.json",
-        );
-        const x = model.predict(reshapedTensor) as tf.Tensor; // await?
-
-        x.array().then((array) => {
-          const predictedWx = array[0][23][0] * std.Wx + mean.Wx;
-          const predictedWy = array[0][23][1] * std.Wy + mean.Wy;
-
-          const predictedDegrees =
-            Math.atan2(predictedWx, predictedWy) * (180 / Math.PI);
-          const predictedSpeed = Math.sqrt(
-            Math.pow(predictedWx, 2) + Math.pow(predictedWy, 2),
-          );
-
-          const predictedTemp = array[0][23][2] * std.Temp + mean.Temp;
-          const obj = {
-            predictedDegrees,
-            predictedSpeed,
-            predictedTemp,
-            actualDegrees: dataArray[0].wdir,
-            actualSpeed: dataArray[0].wspd,
-            actualTemp: dataArray[0].temp,
-          };
-          resolve(obj);
-        });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
