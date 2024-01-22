c08a3d3f13963164a9fe211516b68f2783ea5aa3
MartinTAngell <marty@angellbros.com>
2024-01-21 21:04:45 -0500

    Switched to having about link to github and added systemdesign.md

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,13 @@ const Header = () => {
     <header>
       <div id="headerLinks">
         <a href="/">Forecast</a>
-        <a href="/about">About</a>
+        <a
+          href="https://github.com/LanderDuncan/LSTMWeather/blob/main/systemdesign.md"
+          target="_blank"
+          rel="noreferrer"
+        >
+          About
+        </a>
       </div>
       <div id="githubLogo">
         <a
